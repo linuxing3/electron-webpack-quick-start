075fe1797f932df9f2d6a8b97fb67dc1fb7f06e7
linuxing3 <linuxing3@qq.com>
2020-06-20 21:49:20 -0400

    dynamic render

--- src/renderer/pages/save.form.page.tsx
+++ src/renderer/pages/save.form.page.tsx
@@ -7,108 +7,116 @@ import { GlobalContext } from "./main.page";
 
 const SaveForm = () => {
   const history = useHistory();
-  const { state: { table, currentItem, token } } = React.useContext(GlobalContext);
-  const options = token? {
-    headers: {
-      'Authorization': token
-    }
-  } : {}
-  // deep clone
-  const initialValues= JSON.parse(JSON.stringify(currentItem));
+  const {
+    state: { table, currentItem, token },
+    changeState
+  } = React.useContext(GlobalContext);
+  const options = token
+    ? {
+        headers: {
+          Authorization: token
+        }
+      }
+    : {};
   // const itemStore = window.localStorage.getItem("currentItem");
 
-  const update = async (data: any, where: any) => {
-    const url =
-      table === "users"
-        ? `/api/v1/${table}/${where.id}?table=${table}`
-        : `/api/v1/${table}/${where.id}`;
-    await axios(url, {
-      method: "put",
-      data: JSON.stringify(skipFields(data)),
-      ...options
-    });
-    history.push('/list')
+  const [tableField, setTableField] = React.useState([]);
+  const fetchFields = async () => {
+    const url = `/api/v1/fields/${table}`;
+    const response = await axios.get(url, options);
+    const tableFields = await response.data.data;
+    setTableField(tableFields);
   };
 
-  const remove = async (where: any) => {
+  const update = async () => {
     const url =
       table === "users"
-        ? `/api/v1/${table}/${where.id}?table=${table}`
-        : `/api/v1/${table}/${where.id}`;
+        ? `/api/v1/${table}/${currentItem.id}?table=${table}`
+        : `/api/v1/${table}/${currentItem.id}`;
+    await axios(url, {
+      method: "put",
+      data: JSON.stringify(skipFields(currentItem)),
+      ...options
+    });
+    clear();
+  };
+
+  const clear = () => {
+    changeState({ currentItem: {} });
+    history.push("/list");
+  };
+
+  const remove = async () => {
+    const url =
+      table === "users"
+        ? `/api/v1/${table}/${currentItem.id}?table=${table}`
+        : `/api/v1/${table}/${currentItem.id}`;
     await axios(url, {
       method: "delete",
       ...options
     });
-    history.push('/list')
+    clear();
   };
 
-  /**
-   * 使用状态中的数据，初始化表单
-   */
-  const { values, handleChange } = useForm({
-    initialValues,
-    onSubmit: ({ values }) => console.log(values)
-  });
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    e.persist();
+    const newItem = { [name]: value };
+    changeState(newItem);
+  };
 
+  React.useEffect(() => {
+    fetchFields();
+  }, [table]);
+
+  const renderInputs = () => {
+    return (
+      <>
+        {tableField.map((field) => {
+          return (
+            <div key={field.name} className='mb-4'>
+              <label className='block text-gray-700 text-sm font-bold mb-2'>
+                {field.name}
+              </label>
+              <input
+                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                name={field.name}
+                value={currentItem[field.name]}
+                onChange={handleChange}
+              ></input>
+              {/* {errors.name && <span>This field is required</span>} */}
+            </div>
+          );
+        })}
+      </>
+    );
+  };
+
+  const renderActions = () => (
+    <div className='flex items-center justify-between'>
+      <button
+        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        type='submit'
+      >
+        update
+      </button>
+      <button
+        className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
+        onClick={remove}
+      >
+        Remove
+      </button>
+    </div>
+  );
+  
   return (
     <div className='flex mb-4 justify-center py-30'>
-      <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 my-8 mb-4'>
-        <div className='mb-4'>
-          <label className='block text-gray-700 text-sm font-bold mb-2'>
-            {values.id}
-          </label>
-        </div>
-        <div className='mb-4'>
-          <label className='block text-gray-700 text-sm font-bold mb-2'>
-            Username
-          </label>
-          <input
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-            name='name'
-            value={values.name}
-            onChange={handleChange}
-          ></input>
-          {/* {errors.name && <span>This field is required</span>} */}
-        </div>
-        <div>
-          <label className='block text-gray-700 text-sm font-bold mb-2'>
-            Email
-          </label>
-          <input
-            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-            name='email'
-            value={values.email}
-            onChange={handleChange}
-          ></input>
-          {/* {errors.email && <span>This field is required</span>} */}
-        </div>
-        <div className='mb-6'>
-          <label className='block text-gray-700 text-sm font-bold mb-2'>
-            Password
-          </label>
-          <input
-            className='shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline'
-            name='password'
-            value={values.password}
-            onChange={handleChange}
-            type='password'
-          ></input>
-          {/* {errors.password && <span>This field is required</span>} */}
-        </div>
-        <div className='flex items-center justify-between'>
-          <button
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-            onClick={() => update(values, { id: values.id })}
-          >
-            update
-          </button>
-          <button
-            className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-            onClick={() => remove({ id: values.id })}
-          >
-            Remove
-          </button>
-        </div>
+      <form
+        onSubmit={update}
+        className='bg-white shadow-md rounded px-8 pt-6 pb-8 my-8 mb-4'
+      >
+        {renderInputs()}
+        {renderActions()}
       </form>
     </div>
   );
--- src/renderer/pages/user.list.page.tsx
+++ src/renderer/pages/user.list.page.tsx
@@ -5,22 +5,35 @@ import axios from "../helpers/axios.client";
 import { GlobalContext } from "./main.page";
 
 const LoginList = () => {
-  const { state: { table, token }, changeState } = React.useContext(GlobalContext);
-  const options = token? {
-    headers: {
-      'Authorization': token
-    }
-  } : {}
+  const {
+    state: { table, token },
+    changeState
+  } = React.useContext(GlobalContext);
+  const options = token
+    ? {
+        headers: {
+          Authorization: token
+        }
+      }
+    : {};
 
   const [list, setList] = React.useState([]);
+  const [tableField, setTableField] = React.useState([]);
 
   const history = useHistory();
 
-  const findAll = async () => {
+  const fetchFields = async () => {
+    const url = `/api/v1/fields/${table}`;
+    const response = await axios.get(url, options);
+    const tableFields = await response.data.data;
+    setTableField(tableFields);
+  };
+
+  const fetchData = async () => {
     const url =
-    table === "users"
-      ? `/api/v1/${table}?table=${table}`
-      : `/api/v1/${table}`;
+      table === "users"
+        ? `/api/v1/${table}?table=${table}`
+        : `/api/v1/${table}`;
     const response = await axios.get(url, options);
     const data = await response.data.data;
     setList(data);
@@ -28,57 +41,74 @@ const LoginList = () => {
 
   const showDetail = (id: any) => {
     const currentItem = list.filter((i) => i.id === id)[0];
-    window.localStorage.setItem("currentItem", JSON.stringify(currentItem) );
+    window.localStorage.setItem("currentItem", JSON.stringify(currentItem));
     changeState({ currentItem });
     history.push(`/save/${id}`);
   };
 
   React.useEffect(() => {
-    findAll();
+    fetchFields();
+    fetchData();
   }, [table]);
+
+  const renderFields = (item) => {
+    return (
+      <div className='text-sm'>
+        {tableField.map((field) => {
+          return (
+            <div key={field.name}>
+              <p className='text-gray-900 leading-none'>{field.name}</p>
+              <p className='text-gray-600'>{item[field.name]}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const renderCard = (item) => {
+    return (
+      <div
+        onClick={() => showDetail(item.id)}
+        key={item.id}
+        className='max-full-xs rounded overflow-hidden shadow-lg py-30'
+      >
+        <img
+          className='w-full'
+          src='https://tailwindcss.com/img/card-top.jpg'
+          alt='Sunset in the mountains'
+        />
+        <div className='px-6 py-4'>
+          <div className='font-bold text-xl mb-2'>{item.name}</div>
+          <p className='text-gray-700 text-base'>{item.email}</p>
+        </div>
+        <div className='px-6 py-4 flex items-center'>
+          <img
+            className='w-10 h-10 rounded-full mr-4'
+            src='https://tailwindcss.com/img/jonathan.jpg'
+            alt='Avatar of Jonathan Reinink'
+          />
+          {renderFields(item)}
+        </div>
+        <div className='px-6 py-4'>
+          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+            #react
+          </span>
+          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
+            #typescript
+          </span>
+          <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
+            #electron
+          </span>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className='flex flex-wrap mb-4 justify-center py-30'>
       {list.map((item) => {
-        return (
-          <div
-            onClick={() => showDetail(item.id)}
-            key={item.id}
-            className='max-full-xs rounded overflow-hidden shadow-lg py-30'
-          >
-            <img
-              className='w-full'
-              src='https://tailwindcss.com/img/card-top.jpg'
-              alt='Sunset in the mountains'
-            />
-            <div className='px-6 py-4'>
-              <div className='font-bold text-xl mb-2'>{item.name}</div>
-              <p className='text-gray-700 text-base'>{item.email}</p>
-            </div>
-            <div className='px-6 py-4 flex items-center'>
-              <img
-                className='w-10 h-10 rounded-full mr-4'
-                src='https://tailwindcss.com/img/jonathan.jpg'
-                alt='Avatar of Jonathan Reinink'
-              />
-              <div className='text-sm'>
-                <p className='text-gray-900 leading-none'>{item.name}</p>
-                <p className='text-gray-600'>{item.name}</p>
-              </div>
-            </div>
-            <div className='px-6 py-4'>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-                #react
-              </span>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-                #typescript
-              </span>
-              <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
-                #electron
-              </span>
-            </div>
-          </div>
-        );
+        renderCard(item);
       })}
     </div>
   );
